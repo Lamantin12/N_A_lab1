dc810bb5081eeb3726b981799c1ae023ba2e228d
lamantin12 <Khodosevich.Leo@gmail.com>
2022-06-29 19:51:22 +0300

    Added all .py files

--- lab1_task2.py
+++ lab1_task2.py
@@ -1,29 +1,59 @@
 from numpy import arctan, float64, format_float_scientific
 
 def function(x):
+    """
+    Function counting arctan in dot x
+    :param x:float
+    :return:float
+    """
     return arctan(x)
 
 def n_teylor(x, n):
+    """
+    n-part of arctan teylor sum in dot x
+    :param x:float
+    :param n:int
+    :return:float
+    """
     return (-1)**(n-1) * x**(2*n - 1) / (2*n - 1)
 
 def sum_of_taylor(x, n):
+    """
+    n-th teylor sum in dot x
+    :param x:float
+    :param n:int
+    :return:float
+    """
     res = 0
     for i in range(1,n+1,1):
         res += n_teylor(x, i)
     return res
 
 def abs_error(dx, x):
+    """
+    Function counting absolute error between x and dx: |dx - x|
+    :param dx:float
+    :param x:float
+    :return:float
+    """
     return abs(dx - x)
 
 def relative_error(dx, x):
-    return abs_error(dx, x) / dx
+    """
+    Function counting relative error between x and dx: |dx - x|/dx
+    :param dx:float
+    :param x:float
+    :return:float
+    """
+    return abs(dx, x) / dx
 
 def N_reaching_error_in_dot(dot, epsilon):
     """
-    :param dot: dot where we find N'th sum that reaches error
-    :param epsilon: acceptable error in dot
-    :return: int n
-            number of elements in sum that fits in error
+    Number of elements in Sum that fits in error epsilon in dot
+    :param dot:float
+    :param epsilon:float
+    :return:n:int
+
     """
     Sum = n_teylor(dot, 1)
     n = 2
@@ -35,7 +65,13 @@ def N_reaching_error_in_dot(dot, epsilon):
     return n
 
 
-def ROUND(x, t=4):
+def ROUND(x, t = 4):
+    """
+    Function rounding mantissa of x to t signs
+    :param x:float
+    :param t:int
+    :return:float
+    """
     return float64(format_float_scientific(x, precision=t))
 
 def S_n_rounded_el(x, n):
@@ -45,6 +81,12 @@ def S_n_rounded_el(x, n):
         res = ROUND(res, 6)
     return res
 def S_n_rounded(x, n):
+    """
+    n-th teylor sum in dot x where every part is rounded
+    :param x:float
+    :param n:int
+    :return:float
+    """
     res = x.copy()
     for i in range(len(x)):
         res[i] = S_n_rounded_el(x[i], n)
